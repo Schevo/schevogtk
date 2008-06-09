c4fe555f5db4b346964a03f8c1cda9194f6e92c0
Matthew Scott <gldnspud@gmail.com>
2008-06-08 22:32:51 -0700

    Nicer error message for TransactionRuleViolation

--- schevogtk2/error.py
+++ schevogtk2/error.py
@@ -14,35 +14,54 @@ import schevo.error
 BULLET = u'\u2022 '
 
 
+def escape(s):
+    return s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
+
+
 def show_error(parent, e):
     # By default, just show the error message verbatim.
     markup = [str(e)]
     # Override for specific error types.
     if isinstance(e, schevo.error.DeleteRestricted):
+        print 'DeleteRestricted'
         markup = [
-            u'You cannot delete this object from the database.\n'
-            u'It is referred to by the following objects:\n'
+            u'The object was not deleted from the database.\n'
+            u'\n'
+            u'It is referred to by the following objects,\n'
+            u'which require you to delete them first before\n'
+            u'deleting this object.\n'
             u'\n'
             ]
         for entity, referring_entity, referring_field_name in e.restrictions:
-            markup.append(BULLET + unicode(entity) + '\n')
+            markup.append(BULLET + '<b>%s</b>\n' % escape(unicode(entity)))
     elif isinstance(e, schevo.error.KeyCollision):
+        print 'KeyCollision'
         markup = [
-            u'You cannot save this object to the database.\n'
+            u'Your changes were not saved.\n'
+            u'\n'
             u'There is already an object of this type that has\n'
             u'the following values, which must be unique:\n'
             u'\n'
             ]
         for field_name, field_value in zip(e.key_spec, e.field_values):
             markup.append(BULLET + '<b>%s</b>: %s'
-                          % (field_name, field_value))
+                          % (escape(field_name), escape(field_value)))
+    elif isinstance(e, schevo.error.TransactionRuleViolation):
+        print 'TransactionRuleViolation'
+        markup = [
+            u'Your changes were not saved.\n'
+            u'\n'
+            u'The following transaction rule was violated when\n'
+            u'attempting to save:\n'
+            u'\n'
+            ]
+        markup.append(BULLET + u'<b>%s</b>\n' % escape(e.message))
     markup = u''.join(markup)
     # Show the dialog.
     flags=gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT
     win = gtk.MessageDialog(parent=parent, flags=flags,
                             type=gtk.MESSAGE_ERROR,
-                            buttons=gtk.BUTTONS_CLOSE,
-                            message_format=str(e))
+                            buttons=gtk.BUTTONS_CLOSE)
     win.set_markup(markup)
     win.run()
     win.destroy()
