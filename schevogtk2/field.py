79f7d49f94979d1f4fb148f0a070f81a55f49a48
Matthew R. Scott <gldnspud@gmail.com>
2008-07-14 15:19:49 -0700

    Set initial label properly for boolean field widgets
    Signed-off-by: Matthew R. Scott <gldnspud@gmail.com>

--- schevogtk2/field.py
+++ schevogtk2/field.py
@@ -303,13 +303,14 @@ def _set_field_rw_boolean(container, db, field, change_cb):
         if value is UNASSIGNED:
             value = False
         widget.set_active(value)
-        widget.set_label(unicode(value))
         def on_toggled(widget):
             active = widget.get_active()
             if active:
                 widget.set_label(field.true_label)
             else:
                 widget.set_label(field.false_label)
+        # Set initial label.
+        on_toggled(widget)
         widget.connect('toggled', on_toggled)
         widget.connect('toggled', change_cb, field)
         return (False, widget, None)
