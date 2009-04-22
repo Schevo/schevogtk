4fff9a593d30c415e03a14b38b80598fa93409fa
Matthew R. Scott <gldnspud@gmail.com>
2009-04-21 21:10:12 -0700

    Copyright assignment.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -36,7 +36,7 @@ master_doc = 'index'
 
 # General substitutions.
 project = 'SchevoGtk'
-copyright = '2008, Orbtech, L.L.C.'
+copyright = '2009, ElevenCraft Inc.'
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
@@ -158,7 +158,7 @@ htmlhelp_basename = 'SchevoGtkdoc'
 # (source start file, target name, title, author, document class [howto/manual]).
 latex_documents = [
   ('index', 'SchevoGtk.tex', 'SchevoGtk Documentation',
-   'Orbtech, L.L.C.', 'manual'),
+   'ElevenCraft Inc.', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
